19632ac286d18484070790793bf13b16f9f54523
springhana <xksxksanfro7@naver.com>
2023-03-11 05:11:47 +0900

    Please

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
-          element={<Detail />}
-        ></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+
 function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>

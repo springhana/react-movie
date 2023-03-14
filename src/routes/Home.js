ebfed99b684d008cf9e5023a450d0c867b5e4167
springhana <xksxksanfro7@naver.com>
2023-03-14 09:29:29 +0900

    Movie

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,7 @@ function Home() {
   const getMovie = async () => {
     const json = await (
       await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+        "https://yts.mx/api/v2/list_movies.json?&sort_by=year&genre=animation&limit=20&page=2"
       )
     ).json();
     setMovies(json.data.movies);
